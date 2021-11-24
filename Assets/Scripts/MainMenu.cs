b91da5f17e03ee5eab4300e1f4c199c743cf302d
winchell459 <johncmorris@gmail.com>
2021-11-24 13:52:21 -0800

    end of class 11/19/21

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartButton()
     {
-        
+        SceneManager.LoadScene("Chess");
+    }
+    public void SettingsButton()
+    {
+        SceneManager.LoadScene("Settings");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreditsButton()
     {
-        
+        SceneManager.LoadScene("Credits");
     }
 }
