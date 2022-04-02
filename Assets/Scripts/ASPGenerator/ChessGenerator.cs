2fc5ea9f1f303899373912052bf1d63f9b786c61
winchell459 <johncmorris@gmail.com>
2022-04-01 21:14:34 -0700

    pawns and kings placed

--- Assets/Scripts/ASPGenerator/ChessGenerator.cs
+++ Assets/Scripts/ASPGenerator/ChessGenerator.cs
@@ -17,18 +17,61 @@ public class ChessGenerator : CheckeredPathGenerator
 
     string piecePathRules { get { return generatePiecePathRules(); } }
 
+    //string generatePiecePathRules()
+    //{
+    //    string aspCode = $@"
+
+    //        chess_pieces(king_black;king_white).
+    //        0{{piece_start(XX,YY,Type): chess_pieces(Type)}}1 :- tile(XX,YY,{tile_types.filled}).
+    //        piece_path(XX,YY) :- piece_start(XX,YY,_).
+
+    //    ";
+
+    //    return aspCode;
+    //}
+
     string generatePiecePathRules()
     {
         string aspCode = $@"
-
-            chess_pieces(king_black;king_white).
-            0{{piece_start(XX,YY,Type): chess_pieces(Type)}}1 :- tile(XX,YY,{tile_types.filled}).
+            
             piece_path(XX,YY) :- piece_start(XX,YY,_).
+
+            1{{piece_start(XX,YY,king_black): tile(XX,YY,{tile_types.filled})}}1.
+            1{{piece_start(XX,YY,king_white): tile(XX,YY,{tile_types.filled})}}1.
+
+            king_white_path(XX,YY) :- piece_start(XX,YY,king_white).
+            king_black_path(XX,YY) :- piece_start(XX,YY,king_black).
+
+            piece_path(XX,YY) :- king_white_path(XX,YY).
+            king_white_path(XX,YY) :- king_white_path(XX-1,YY), tile(XX,YY,{tile_types.filled}).
+            king_white_path(XX,YY) :- king_white_path(XX+1,YY), tile(XX,YY,{tile_types.filled}).
+            king_white_path(XX,YY) :- king_white_path(XX,YY-1), tile(XX,YY,{tile_types.filled}).
+            king_white_path(XX,YY) :- king_white_path(XX,YY+1), tile(XX,YY,{tile_types.filled}).
+
+            piece_path(XX,YY) :- king_black_path(XX,YY).
+            king_black_path(XX,YY) :- king_black_path(XX-1,YY), tile(XX,YY,{tile_types.filled}).
+            king_black_path(XX,YY) :- king_black_path(XX+1,YY), tile(XX,YY,{tile_types.filled}).
+            king_black_path(XX,YY) :- king_black_path(XX,YY-1), tile(XX,YY,{tile_types.filled}).
+            king_black_path(XX,YY) :- king_black_path(XX,YY+1), tile(XX,YY,{tile_types.filled}).
+
+            8{{piece_start(XX,YY,pawn_black): tile(XX,YY,{tile_types.filled})}}8.
+            8{{piece_start(XX,YY,pawn_white): tile(XX,YY,{tile_types.filled})}}8.
+
+            piece_path(XX,YY) :- pawn_white_path(XX,YY).
+            pawn_white_path(XX,YY) :- piece_start(XX,YY,pawn_white).
+            pawn_white_path(XX,YY) :- pawn_white_path(XX,YY+1), tile(XX,YY,{tile_types.filled}).
+
+            piece_path(XX,YY) :- pawn_black_path(XX,YY).
+            pawn_black_path(XX,YY) :- piece_start(XX,YY,pawn_black).
+            pawn_black_path(XX,YY) :- pawn_black_path(XX,YY-1), tile(XX,YY,{tile_types.filled}).
+
+
+            :- piece_start(XX,YY, T1), piece_start(XX,YY, T2), T1 != T2.
+
+            
 
         ";
 
         return aspCode;
     }
-
-    
 }
